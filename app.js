f6b11f6166b7c894e7576d207d02721389ec24a8
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-14 12:25:20 +0300

    refactor: changed localhost for db

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
