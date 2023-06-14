50d47ac3a5e6ba5d874d8c0e9b9de4d93382102e
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-14 14:18:58 +0300

    refactor: add env check to jwt

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,8 +1,12 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const JWT_SECRET = 'someveryveryverysecretkey';
+const { NODE_ENV, JWT_SECRET } = process.env;
+const devsecret = 'secret-key';
 
-const getJwtToken = (id) => jwt.sign({ id }, JWT_SECRET, { expiresIn: '7d' });
-const verifyJwtToken = (token) => jwt.verify(token, JWT_SECRET);
+const secretToken = NODE_ENV === 'production' ? JWT_SECRET : devsecret;
+
+const getJwtToken = (id) => jwt.sign({ id }, secretToken, { expiresIn: '7d' });
+const verifyJwtToken = (token) => jwt.verify(token, secretToken);
 
 module.exports = { getJwtToken, verifyJwtToken };
