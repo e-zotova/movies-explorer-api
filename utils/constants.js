dc41a5fbc57b0e41862e6f94fc4a790a0453200d
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-14 16:30:31 +0300

    fix: update userNotFound message

--- utils/constants.js
+++ utils/constants.js
@@ -1,12 +1,15 @@
 const validationError = 'ValidationError';
+const notFoundError = 'DocumentNotFoundError';
+const conflictError = 11000;
+
 const invalidDataMovieMessage = 'Invalid data when creating a movie';
 const invalidDataUserMessage = 'Invalid data when creating a user';
 const invalidIdUserMessage = 'Invalid user id';
-const notFoundError = 'DocumentNotFoundError';
+
 const movieNotFoundMessage = 'Movie is not found';
-const userNotFoundMessage = 'DocumentNotFoundError';
+const userNotFoundMessage = 'User is not found';
+
 const deleteMovieForbiddenMessage = 'It is not allowed to delete other user\'s movie';
-const conflictError = 11000;
 const userConflictMessage = 'User already exists';
 
 module.exports = {
